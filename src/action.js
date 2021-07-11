b8d176a6dd62cf7e3ee606444229ab4ed6f4c435
Sharan Aithal <32029982+sharan-aithal@users.noreply.github.com>
2021-07-11 11:17:20 +0530

    greet the contributor

--- src/action.js
+++ src/action.js
@@ -1,5 +1,22 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
 async function run() {
-  console.log('Hello World');
+  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+  const message = core.getInput('message');
+
+  const { context = {} } = github;
+  const { pull_request } = context.payload;
+
+  console.log('Pull Request #${pull_request.number}');
+
+  const octokit = github.getOctokit(GITHUB_TOKEN);
+
+  await octokit.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: `${message}`
+  });
 }
 
 run();
